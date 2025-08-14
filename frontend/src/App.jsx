8d4b8cb0c2c552a6cc53038b0367b7435f0001e1
Karthikeyan1701 <karthikvijay171@gmail.com>
2025-08-14 13:18:27 +0530

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/user-facing/Home';
+import Movies from './pages/user-facing/Movies';
+import MovieInfo from './pages/user-facing/MovieInfo';
+import Reviews from './pages/user-facing/Reviews';
+import ReviewDetails from './pages/user-facing/ReviewDetails';
+import SearchResults from './pages/user-facing/SearchResults';
 
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-600">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movie-info" element={<MovieInfo />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/review-details" element={<ReviewDetails />} />
+        <Route path="/search-results" element={<SearchResults />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
